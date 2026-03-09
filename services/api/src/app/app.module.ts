d6622e4183da0144551aa3f78bb5770dd806192c
saher <saherdev4@gmail.com>
2026-03-09 16:53:13 +0700

    feat(api): register Clerk middleware globally in AppModule

--- services/api/src/app/app.module.ts
+++ services/api/src/app/app.module.ts
@@ -1,10 +1,15 @@
-import { Module } from '@nestjs/common';
+import {
+  Module,
+  MiddlewareConsumer,
+  NestModule,
+} from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import * as path from 'path';
 import * as yaml from 'yaml';
 import * as fs from 'fs';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClerkMiddleware } from '../common/middleware/clerk.middleware';
 
 const configPath = path.join(__dirname, '../../config/.local.yaml');
 
@@ -23,4 +28,8 @@ const configPath = path.join(__dirname, '../../config/.local.yaml');
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(ClerkMiddleware).forRoutes('*');
+  }
+}
